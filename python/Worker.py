6e33025b727c626bb85e4179eda9c24038cad050
Martin Bergemann <martin.bergemann@monash.edu>
2018-06-19 17:30:01 +1000

    Check of exit-status of bash cmds

--- python/Worker.py
+++ python/Worker.py
@@ -11,12 +11,18 @@ def um2nc(expID, thread, *args, **kwargs):
     to a meaningful name
     '''
     try:
-        res = args[0]
+        res = args[1]
     except IndexError:
         res = '0p44km'
+
     fileID = dict(ph='rain', pg='geop_th', pf='pres_th', pe='qcl_th',
                   pd='qcf_th', pc='vert_cent', pb='vert_wind', pa='surf',
                   pvera='vera', pverb='verb', pverc='verc')
+    try:
+      fileID = {args[0]:fileID[args[0]]}
+    except IndexError:
+      fileID = fileID
+
     expID_tmp = expID.replace('u-','u-2006')
     date = datetime.strptime(expID_tmp, 'u-%Y%m%d%H%M')
     umID = date.strftime('%Y%m%dT%H%MZ')
@@ -27,13 +33,12 @@ def um2nc(expID, thread, *args, **kwargs):
     if not os.path.isdir(path):
         print('%s : path does not exist'%path)
         return 1
-
     os.chdir(path)
     for umid, ncid in fileID.items():
         outdates = []
-        ii = 0
-        umfiles = glob('umnsaa_%s'%umid)
+        umfiles = glob('umnsaa_%s*'%umid)
         umfiles.sort()
+        merge = True
         for umfile in umfiles:
             testfile = glob('um-%s-%s-%s_????????_????-????????_????.nc'\
             %(res, expID.replace('u-',''), ncid))
@@ -41,7 +46,11 @@ def um2nc(expID, thread, *args, **kwargs):
                 merge = True
                 cmd='um2cdf %s'%umfile
                 print('%s: %s'%(thread, cmd))
-                os.system(cmd)
+                ret = os.system(cmd)
+                if ret != 0:
+                  print('%s: Error moving'%thread)
+                  os.system("echo %s $PWD"%thread)
+                  return 1
                 num = int(umfile.split('_')[-1].replace(umid,''))
                 outdate = (date + timedelta(hours=num)).strftime('%Y%m%d_%H%M')
                 outfile = 'um-%s-%s-%s_%s.nc'%(res, expID.replace('u-',''), ncid,
@@ -49,12 +58,13 @@ def um2nc(expID, thread, *args, **kwargs):
                 cmd2 = 'mv %s.nc %s' %(umfile, outfile)
                 outdates.append(outdate)
                 print('%s: %s'%(thread, cmd2))
-                os.system(cmd2)
+                ret = os.system(cmd2)
+                if ret != 0:
+                  print('%s: Error moving'%thread)
+                  os.system("echo %s $PWD"%thread)
+                  return 1
             else:
                 merge = False
-            ii += 1
-            if ii > 2:
-                break
 
         outdates.sort()
         if merge:
