c52fae53606009c5fba203d1d372a55af0dd1813
Martin Bergemann <martin.bergemann@monash.edu>
2018-09-18 06:35:48 +0200

    Default theme

--- python/make_jp-slides.sh
+++ python/make_jp-slides.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 jupyter nbconvert $1 --to slides --post serve \
-   --SlidesExporter.reveal_theme=sky \
+   --SlidesExporter.reveal_theme=default \
    --SlidesExporter.reveal_scroll=True \
    --SlidesExporter.reveal_transition=none
