8bc093de405dd2d5d36c0f2c68a6421a621a77cf
Martin Bergemann <martin.bergemann@monash.edu>
2018-11-20 05:30:00 +0100

    Added 3D mass-flux integral

--- python/storm_prop.py
+++ python/storm_prop.py
@@ -48,6 +48,10 @@ def calc_rho(q, T, p):
     rho = pd/(Rd*T) + pv/(Rv*T)
     return rho
 
+def calc_z(p0, p, T):
+    
+    return (((p0/p)**(1/5.257) - 1) * T) / 0.0065
+
 def q(files, loc, **kwargs):
     '''Get humidity of a location'''
     tidx, loci, locj = loc #Index
@@ -149,6 +153,46 @@ def mflux(files, loc, **kwargs):
     f.close()
     g.close()
     return mflux, lats, lons, np.array([time[0]])
+
+def moist_int(files, loc, **kwargs):
+    '''Moisture flux vol integral'''
+
+    qf = xr.open_dataset(files['vert_cent'])
+    vf = xr.open_dataset(files['vert_wind'])
+    sf = xr.open_dataset(files['surf'])
+    tidx, loci, locj = loc
+    w = vf['dz_dt'][tidx, :, loci[0]:loci[1], locj[0]:locj[1]].values
+    v = vf['v'][tidx, :, loci[0]:loci[1], locj[0]:locj[1]].values
+    u = vf['u'][tidx, :, loci[0]:loci[1], locj[0]:locj[1]].values
+    q = qf['q'][tidx, :, loci[0]:loci[1], locj[0]:locj[1]].values
+    T = qf['temp'][tidx, :, loci[0]:loci[1], locj[0]:locj[1]].values
+    sp = sf['p'][tidx, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    P = vf['p'][:].values.reshape(-1, 1, 1) * 100.
+    try:
+        lons = vf['lon'][locj[0]:locj[1]].values
+        lats = vf['lat'][loci[0]:loci[1]].values
+    except KeyError:
+        lons = vf['longitude'][locj[0]:locj[1]].values
+        lats = vf['latitude'][loci[0]:loci[1]].values
+
+    time = date2num(pd.DatetimeIndex(vf['t'][tidx:tidx+1].values).to_pydatetime(),
+                    'seconds since 1970-01-01 00:00:00')
+    time = np.array([time[0]])
+    Z = calc_z(sp, P, T).mean(axis=(-2, -1))
+    dy = cdist((lons[0], lats[0]), (lons[0], lats[1]))*1000
+    dx = cdist((lons[0], lats[0]), (lons[1], lats[0]))*1000
+    dqvdy = np.gradient(v, Z, dy, dx)[1]
+    dqudx = np.gradient(u, Z, dy, dx)[2]
+    dqwdz = np.gradient(w, Z, dy, dx)[0]
+
+    div = dqwdz + dqvdy + dqudx
+
+    vf.close(), qf.close(), sf.close()
+
+    return div, lats, lons, np.array([time[0]])
+
+
+
 
 
 def mfluxdiv(files, loc, **kwargs):
@@ -276,32 +320,111 @@ def cloud_z(files, loc, **kwargs):
     f.close(), g.close(), z.close()
     return interp(qcl+qcf, ht, Z), lats, lons, np.array([time[0]])
 
+def z_lev(files, loc, **kwargs):
+    '''Get the z level'''
 
-def entrainment(files, loc, **kwargs):
+    v = xr.open_dataset(files['vert_cent'])
+    z = xr.open_dataset(files['surf'])
+
+    P = v['p'][:].values * 100
+
+    tidx, loci, locj = loc #Index
+    sp = z['p'][tidx, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    T = v['temp'][tidx, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    Z = calc_z(sp, P.reshape(-1,1,1), T)
+
+    try:
+        lons = v['lon'][locj[0]:locj[1]].values
+        lats = v['lat'][loci[0]:loci[1]].values
+    except KeyError:
+        lons = v['longitude'][locj[0]:locj[1]].values
+        lats = v['latitude'][loci[0]:loci[1]].values
+
+    time = date2num(pd.DatetimeIndex(v['t'][tidx:tidx+1].values).to_pydatetime(),
+                    'seconds since 1970-01-01 00:00:00')
+    v.close(), z.close()
+
+    return Z, lats, lons, np.array(time[0])
+
+def msep(files, loc, **kwargs):
     '''Calculate entrainment rate'''
     from model2lev import interp
+
+    cpd = 1005.7
+    Lv = 2.51e6
+    Lf = 2.83e6
+    ff = xr.open_dataset(files['qcl_th']) 
+    gf = xr.open_dataset(files['qcf_th'])
+    hf = xr.open_dataset(files['surf'])
+    fi = xr.open_dataset(files['res_th'])
+    jf = xr.open_dataset(files['vert_cent'])
+    kf = xr.open_dataset(files['vert_wind'])
+    P = jf['p'][:].values * 100
+    tidx, loci, locj = loc #Index
+    try:
+        tdelta = int(kwargs['tdelta'])
+    except KeyError:
+        tdelta = 1
+
+
+
+    # Get the time windox from timedelta (tdelta in hours)
+    time = pd.DatetimeIndex(jf['t'][:].values)
+    start = time[tidx] - timedelta(hours=tdelta)
+    end = time[tidx] + timedelta(hours=tdelta)
+    ttidx = np.where((time>= start)  & (time<= end))[0]
+    t1, t2 = ttidx[0], ttidx[-1]+1
+    mt = (t2 - t1) // 2
+
+
+    v = kf['v'][t1:t2, :, loci[0]:loci[1], locj[0]:locj[1]] - kf['v'][t1:t2].mean(axis=(-2, -1))
+    u = kf['u'][t1:t2, :, loci[0]:loci[1], locj[0]:locj[1]] - kf['u'][t1:t2].mean(axis=(-2, -1))
+
+
+    #qcf = g['QCF'][tidx].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    #QF = interp(qcf, p, P[:])
+    p = hf['p'][t1:t2].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    T = jf['temp'][t1:t2].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    q = jf['q'][t1:t2].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    Z = calc_z(p, P.reshape(-1,1,1), T)
+    H = cpd*T + 9.81*Z + Lv*q #- Lf*QF
+    h = H[...,loci[0]:loci[1], locj[0]:locj[1]]
+    h -= np.nanmean(H, axis=(-2, -1)).reshape(-1,len(P), 1, 1)
     
+    vhp = (np.mean(u*h, axis=0) - np.mean(h, axis=0) * np.mean(h, axis=0)).values
+    uhp = (np.mean(v*h, axis=0) - np.mean(h, axis=0) * np.mean(h, axis=0)).values
+    try:
+        lons = jf['lon'][locj[0]:locj[1]].values
+        lats = jf['lat'][loci[0]:loci[1]].values
+    except KeyError:
+        lons = jf['longitude'][locj[0]:locj[1]].values
+        lats = jf['latitude'][loci[0]:loci[1]].values
+
+    time = date2num(pd.DatetimeIndex(jf['t'][tidx:tidx+1].values).to_pydatetime(),
+                    'seconds since 1970-01-01 00:00:00')
+    ff.close(), gf.close(), hf.close(), fi.close(), jf.close(), kf.close()
+    return  np.sqrt(uhp**2 + vhp**2), lats, lons, np.array(time[0])
+
+def mse_bar(files, loc, **kwargs):
+    '''Calculate entrainment rate'''
+    from model2lev import interp
+
     cpd = 1005.7
     Lv = 2.51e6
     Lf = 2.83e6
     f, g = xr.open_dataset(files['qcl_th']), xr.open_dataset(files['qcf_th'])
-    z = xr.open_dataset(files['geop_th'])
+    z = xr.open_dataset(files['surf'])
     pf = xr.open_dataset(files['res_th'])
     v = xr.open_dataset(files['vert_cent'])
     P = v['p'][:].values * 100
     tidx, loci, locj = loc #Index
-    #qcf = g['QCF'][tidx, :, loci[0]:loci[1], locj[0]:locj[-1]].values
-    ht = z['ht'][tidx].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    qcf = g['QCF'][tidx].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
     p = pf['p'][tidx].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
-    Z = interp(ht, p, P[:])
-    #QF = interp(qcf, p, P[:])
+    QF = interp(qcf, p, P[:])
     T = v['temp'][tidx].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
     q = v['q'][tidx].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
-    H = cpd*T[:] + 9.81*Z + Lv*q[:] #- Lf*QF
-    dh = np.diff(H, axis=0)
-    dz = np.diff(Z, axis=0)
-    dhdz = dh[:,loci[0]:loci[1], locj[0]:locj[1]]/dz[:,loci[0]:loci[1], locj[0]:locj[1]]
-    Hp = H[:,loci[0]:loci[1],locj[0]:locj[1]]# - H.mean(axis=(-2, -1)).reshape(-1, 1, 1)
+    Z = calc_z(z['p'][tidx].values, P.reshape(-1,1,1), T)
+    H = cpd*T + 9.81*Z + Lv*q - Lf*QF
     try:
         lons = f['lon'][locj[0]:locj[1]].values
         lats = f['lat'][loci[0]:loci[1]].values
@@ -312,10 +435,41 @@ def entrainment(files, loc, **kwargs):
     time = date2num(pd.DatetimeIndex(f['t'][tidx:tidx+1].values).to_pydatetime(),
                     'seconds since 1970-01-01 00:00:00')
     f.close(), g.close(), z.close(), v.close(), pf.close()
-    e = dhdz/(Hp[1:]+0.01)
-    out = np.full((e.shape[0]+1,e.shape[1], e.shape[2]), 0)
-    out[:-1] = e
-    return out, lats, lons, np.array(time[0])
+    return np.nanmean(H, axis=(-2, -1)).reshape(-1, 1, 1), lats, lons, np.array(time[0])
+
+
+
+def mse(files, loc, **kwargs):
+    '''Calculate entrainment rate'''
+    from model2lev import interp
+
+    cpd = 1005.7
+    Lv = 2.51e6
+    Lf = 2.83e6
+    f, g = xr.open_dataset(files['qcl_th']), xr.open_dataset(files['qcf_th'])
+    z = xr.open_dataset(files['surf'])
+    pf = xr.open_dataset(files['res_th'])
+    v = xr.open_dataset(files['vert_cent'])
+    P = v['p'][:].values * 100
+    tidx, loci, locj = loc #Index
+    qcf = g['QCF'][tidx].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    p = pf['p'][tidx].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    QF = interp(qcf, p, P[:])
+    T = v['temp'][tidx].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    q = v['q'][tidx].values#, :, loci[0]:loci[1], locj[0]:locj[-1]].values
+    Z = calc_z(z['p'][tidx].values, P.reshape(-1,1,1), T)
+    H = cpd*T + 9.81*Z + Lv*q - Lf*QF
+    try:
+        lons = f['lon'][locj[0]:locj[1]].values
+        lats = f['lat'][loci[0]:loci[1]].values
+    except KeyError:
+        lons = f['longitude'][locj[0]:locj[1]].values
+        lats = f['latitude'][loci[0]:loci[1]].values
+
+    time = date2num(pd.DatetimeIndex(f['t'][tidx:tidx+1].values).to_pydatetime(),
+                    'seconds since 1970-01-01 00:00:00')
+    f.close(), g.close(), z.close(), v.close(), pf.close()
+    return H[:,loci[0]:loci[1], locj[0]:locj[1]], lats, lons, np.array(time[0])
 
 
 def momflux(files, loc, **kwargs):
